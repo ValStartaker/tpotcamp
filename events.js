74a17fa1ca4f0547802d75e71a00c5d4569430ce
Dylan Hobby <julesmynyddoedd@gmail.com>
2025-08-14 20:30:38 +0100

    Update event

--- events.js
+++ events.js
@@ -7,7 +7,7 @@ const eventsData = [
     "lat": 32.283157721057904,
     "lng": -64.82098208176512,
     "description": "It's a camp. For meditation nerds. But despite all the meditating it's not a retreat it's a camp so bbq and beers all evening, naturally. Oh and scuba diving. Or just swimming. And talking about meditating instead of actually meditating.",
-    "address": "Central Park, New York, NY",
+    "address": "Ports Island, Bermuda",
     "website": "https://tpot.website/2025/08/14/events-2026/"
   }
 ];
