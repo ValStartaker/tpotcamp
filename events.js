7ddf7707c4defdcb11282eb6a37de987a6a4a123
Dylan Hobby <julesmynyddoedd@gmail.com>
2025-08-14 18:56:43 +0100

    Final

---
+++ events.js
@@ -0,0 +1,24 @@
+const eventsData = [
+  {
+    "name": "Test Event",
+    "type": "camp",
+    "date": "2025-08-18",
+    "time": "18:00",
+    "lat": 40.778879,
+    "lng": -73.967737,
+    "description": "test event",
+    "address": "Central Park, New York, NY",
+    "website": "https://example.com/test-event"
+  },
+  {
+    "name": "Test Event 2",
+    "type": "fractalido",
+    "date": "2025-08-18",
+    "time": "18:00",
+    "lat": 51.501018,
+    "lng": -0.125966,
+    "description": "test event 2",
+    "address": "Westminster, London, UK",
+    "website": "https://example.com/test-event-2"
+  }
+];
---
+++ institutions.js
@@ -0,0 +1,10 @@
+const institutionsData = [
+  {
+    "name": "Test",
+    "description": "Test",
+    "address": "Berkeley, CA",
+    "website": "https://example.com",
+    "contact": "info@example.com"
+  },
+  
+];
