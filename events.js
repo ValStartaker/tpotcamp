9067cfa2b4571220aa989815937703020d2f5a74
Dylan Hobby <julesmynyddoedd@gmail.com>
2025-08-17 12:33:26 +0100

    Added whorehole

--- events.js
+++ events.js
@@ -9,5 +9,16 @@ const eventsData = [
     "description": "It's a camp. For meditation nerds. But despite all the meditating it's not a retreat it's a camp so bbq and beers all evening, naturally. Oh and scuba diving. Or just swimming. And talking about meditating instead of actually meditating.",
     "address": "Ports Island, Bermuda",
     "website": "https://tpot.website/2025/08/14/events-2026/"
+  },
+  {
+    "name": "WhoreHole",
+    "type": "camp",
+    "startDate": "2026-02-16",
+    "endDate": "2026-02-12",
+    "lat": 64.77985668356317,
+    "lng": -21.096083511292992,
+    "description": "WhoreHole is a 5 day, 4 night event in the Icelandic winter wilderness run by @actualwebutante. This event is during the new moon, which is one of the best times to see the Aurora in Iceland. Ticket prices include food, lodging, and transportation. We will bask under the northern lights in a heated pool and soak away shame. The sauna will provide ample space for cozy chats. Workshops provided will promote body positivity, self image, and consent. Expect workshops that will potentially activate and frustrate you. I use the liminality of the remote environment, surreality of 14hr evenings, and the serenity of the snowscape as a harsh catalyst for your transformation. Join me in the arena as we fight your demons.",
+    "address": "Holl House, Siguljodur",
+    "website": "https://www.tickettailor.com/events/whorehole/1822630"
   }
 ];
