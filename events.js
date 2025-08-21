546c707656e6d384530661e4c4e85de3e823cd30
Dylan Hobby <julesmynyddoedd@gmail.com>
2025-08-21 23:27:57 +0100

    Added events

--- events.js
+++ events.js
@@ -18,7 +18,27 @@ const eventsData = [
     "lat": 64.77985668356317,
     "lng": -21.096083511292992,
     "description": "WhoreHole is a 5 day, 4 night event in the Icelandic winter wilderness run by @actualwebutante. This event is during the new moon, which is one of the best times to see the Aurora in Iceland. Ticket prices include food, lodging, and transportation. We will bask under the northern lights in a heated pool and soak away shame. The sauna will provide ample space for cozy chats. Workshops provided will promote body positivity, self image, and consent. Expect workshops that will potentially activate and frustrate you. I use the liminality of the remote environment, surreality of 14hr evenings, and the serenity of the snowscape as a harsh catalyst for your transformation. Join me in the arena as we fight your demons.",
-    "address": "Holl House, Siguljodur",
+    "address": "Holl House, Siguljodur, Iceland",
     "website": "https://www.tickettailor.com/events/whorehole/1822630"
+  },
+  {
+    "name": "Cascade Camp",
+    "type": "camp",
+    "startDate": "2025-09-12",
+    "lat": 46.28615224218717,
+    "lng": -123.4455616740281,
+    "description": "This is our bid for our local pocket of reality to come out of the era of disembodied atomization into a value-aligned network of interdependence. We found each other in the forest of the internet, but it’s no place to root. Cascade Camp is a clearing where we go to practice what it means to belong.  Yes, it’s a little uncomfortable outside, unfiltered, uncovered, undistracted. But that shared discomfort is the soil where real connection grows. We’re becoming people who can sit in paradox, soften without collapsing and build local culture rooted in trust and difference.",
+    "address": "Skamokawa Valley, WA",
+    "website": "https://cascade.camp/"
+  },
+  {
+    "name": "SEACamp",
+    "type": "camp",
+    "startDate": "2025-10-09",
+    "endDate": "2025-10-12",
+    "lat": -8.443638814138877,
+    "lng": 115.1486133349221,
+    "description": "A twitter camp for people in Southeast Asia",
+    "address": "Bali, Indonesia",
   }
 ];
